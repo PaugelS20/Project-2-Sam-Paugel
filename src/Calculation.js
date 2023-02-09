fed2cb97a34ba082dce79c6958f3e3beea2660d4
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-08 22:26:07 -0600

    Now all parts are working
    added first and second number and the + and = to show what goes where!

--- src/Calculation.js
+++ src/Calculation.js
@@ -3,7 +3,7 @@ import './App.css';
 export const Calculation = ({firstNumber, secondNumber}) => {
   return (
    <h1> 
-   {firstNumber + secondNumber}
+    {firstNumber} + {secondNumber} = {firstNumber + secondNumber}
    </h1> 
   );
 }
