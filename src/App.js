0cb857dfc9c1497399fe022d37fbc3750db7029c
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-08 23:55:28 -0600

    Added some more css framework
    Tyding up my project making it look and work great!

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ const App = () => {
 
   return (
    <>
-	<div className='card bg-transparent border-secondary mb-3'>
+   	<div className='container2 m-3'>
+   		<Darkmode/>
+	</div>
+
+	<div className='card bg-transparent border-secondary ms-3 me-3'>
 		<div class="card-body">
-		<Darkmode/>
 		<Calculation firstNumber={1} secondNumber={18} groupNumber={1}/>
 		<Calculation firstNumber={30} secondNumber={100} groupNumber={2}/>
 		<Calculation firstNumber={7} secondNumber={3.5} groupNumber={3}/>
